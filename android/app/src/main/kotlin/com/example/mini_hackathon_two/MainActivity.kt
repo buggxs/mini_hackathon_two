7bebb2823306f1cdbf20b2eab0691771ec141723
Andre Salzmann <32867155+Salzzy@users.noreply.github.com>
2024-07-15 20:54:18 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mini_hackathon_two/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mini_hackathon_two
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:mini_hackathon_two/core/app_service_locator.dart';
+
+void main() {
+  setup();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Bla'),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
